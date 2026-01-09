3fd34bd004ac0e1b54e4617cecc85a6cd18120ab
saad-shaikh01 <119537796+saad-shaikh01@users.noreply.github.com>
2026-01-09 22:56:25 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import "@/styles/globals.css";
 import { cn, generateMetadata } from "@/functions";
 import { inter, satoshi } from "@/constants";
 import { ThemeProvider } from "@/components/global/theme-provider";
-import { Header } from "@/components/layout/Header";
-import { Footer } from "@/components/layout/Footer";
+
 
 export const metadata = generateMetadata();
 
@@ -21,30 +20,14 @@ export default function RootLayout({
           // satoshi.variable
         )}
       >
-        {/* <GoogleAnalytics /> */}
-        <noscript>
-          <a href="https://www.livechat.com/chat-with/13126833/" rel="nofollow">
-            Chat with us
-          </a>
-          , powered by
-          <a
-            href="https://www.livechat.com/?welcome"
-            rel="noopener nofollow"
-            target="_blank"
-          >
-            LiveChat
-          </a>
-        </noscript>
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,rgba(23,23,23,0.4)_1px,transparent_1px),linear-gradient(to_bottom,rgba(23,23,23,0.4)_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-full mt-[63px"></div>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
+         
           {children}
-          <Footer />
         </ThemeProvider>
       </body>
     </html>
